a84bd1428789e7579174c181ad1659fe9596416a
Manu <manuelass.dev@gmail.com>
2024-03-03 11:50:37 -0300

    inicio do projeto

---
+++ index.py
@@ -0,0 +1,13 @@
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+# Imprimindo a quantidade de elementos na lista
+print("Quantidade de elementos na lista:", len(lista_musicos))
+
+# Imprimindo o dado salvo no índice 2
+print("Dado no índice 2:", lista_musicos[2])
+
+# Imprimindo o dado salvo no índice 9
+print("Dado no índice 9:", lista_musicos[9])
+
+# Imprimindo o dado salvo no índice 14
+print("Dado no índice 14:", lista_musicos[14])
